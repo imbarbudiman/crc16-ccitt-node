8c6803bb216ad6879b179d9fb6f86dd1263fcf96
Imbar Budiman <imbarbudiman@yahoo.com>
2016-05-25 02:15:21 +0700

    tambah coment output di contoh.js

--- example/contoh.js
+++ example/contoh.js
@@ -1,6 +1,5 @@
 var crc = require('crc16-ccitt-node');
 var buffData = new Buffer('ff7878', 'hex');
 
-
-console.log("hex : %s ", crc.getCrc16(buffData).toString(16));
-console.log("decimal : %d ", crc.getCrc16(buffData));
+console.log("hex : %s ", crc.getCrc16(buffData).toString(16));	// hex : c1f4
+console.log("decimal : %d ", crc.getCrc16(buffData));			// decimal : 49652
